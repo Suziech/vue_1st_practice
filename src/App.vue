1163d50257e2fc926e3c9d17fc529f197475787e
최슬기 <choisk09@gmail.com>
2022-10-05 12:15:51 +0900

    WIP

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+     <friend-contact></friend-contact>
+     <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from './components/FriendContact.vue';
+export default {
+  components: {
+    FriendContact 
+  },
+  data() {
+    return {
+FriendContact: [
+        {
+          id: "suzie",
+          name: "Suzie Choi",
+          phone: "+6583354565",
+          email: "suzie@gmail.com",
+        },
+        {
+          id: "marco",
+          name: "Macro Hubert",
+          phone: "+65123456789",
+          email: "marco@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "suzie",
+        name: "Suzie Choi",
+        phone: "+6583354565",
+        email: "suzie@gmail.com",
+      },
+    }
+  },
+  methods:{
+    toggleDetails(){
+        this.detailsAreVisible = !this.detailsAreVisible
+    },
+  },
+  computed:{
+    console: ()=> console.log(this.detailsAreVisible)
+  }
+};
+</script>
